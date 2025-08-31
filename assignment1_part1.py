5965e53528e487483a0b97368bbf547fd411fb55
Mohammad <40510808+Rambo917@users.noreply.github.com>
2025-08-31 13:07:44 -0400

    Add files via upload

--- assignment1_part1.py
+++ assignment1_part1.py
@@ -1,19 +1,21 @@
+class ListDivideException(Exception): pass
 
-def list_divide(numbers, divide):
-    """
-    The function returns the number of elements in the numbers list that are divisibleby divide
-    """
-    pass
+def list_divide(numbers, divide=2):
+    return sum(1 for number in numbers if number % divide == 0)
 
 def test_list_divide():
-    """
-    Test listDivide
-    """
-    assert listDivide([1,2,3,4,5]) == 2
-    assert listDivide([2,4,6,8,10]) == 5
-    assert listDivide([30, 54, 63,98, 100], divide=10) == 2
-    assert listDivide([]) == 0
-    assert listDivide([1,2,3,4,5], 1) == 5
-    
+    if list_divide([1,2,3,4,5]) != 2:
+        raise ListDivideException()
+    if list_divide([2,4,6,8,10]) != 5:
+        raise ListDivideException()
+    if list_divide([30, 54, 63,98, 100], divide=10) != 2:
+        raise ListDivideException()
+    if list_divide([]) != 0:
+        raise ListDivideException()
+    if list_divide([1,2,3,4,5], 1) != 5:
+        raise ListDivideException()
+
 if __name__ == "__main__":
-    testListDivide()
+    test_list_divide()
+
+   git commit -m "Add assignment1_part1.py with list_divide and test cases"
